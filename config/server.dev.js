e245fca898d8faf20fa237d4c992ac574f84cb2c
jrsantur <jrsantur06@gmail.com>
2019-09-19 03:17:21 -0500

    first commit

---
+++ config/server.dev.js
@@ -0,0 +1,67 @@
+// config used by server side only
+const dbHost = process.env.DB_HOST || 'ds060977.mlab.com';
+const dbPort = process.env.DB_PORT || 60977;
+const dbName = process.env.DB_NAME || 'jcellphonestore';
+const dbUser = process.env.DB_USER || 'juabsa';
+const dbPass = process.env.DB_PASS || 'Alianza9210';
+const dbCred =
+	dbUser.length > 0 || dbPass.length > 0 ? `${dbUser}:${dbPass}@` : '';
+
+const dbUrl =
+	process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`;
+
+module.exports = {
+	// used by Store (server side)
+	apiBaseUrl: `/api/v1`,
+
+	// used by Store (server and client side)
+	ajaxBaseUrl: `/ajax`,
+
+	// Access-Control-Allow-Origin
+	storeBaseUrl: `http://jcellphone.com`,
+
+	// used by API
+	adminLoginUrl: '/admin/login',
+
+	apiListenPort: 3001,
+	storeListenPort: 3000,
+
+	// used by API
+	mongodbServerUrl: dbUrl,
+
+	smtpServer: {
+		host: '',
+		port: 0,
+		secure: true,
+		user: '',
+		pass: '',
+		fromName: '',
+		fromAddress: ''
+	},
+
+	// key to sign tokens
+	jwtSecretKey: '-',
+
+	// key to sign store cookies
+	cookieSecretKey: '-',
+
+	// path to uploads
+	categoriesUploadPath: 'public/content/images/categories',
+	productsUploadPath: 'public/content/images/products',
+	filesUploadPath: 'public/content',
+	themeAssetsUploadPath: 'theme/assets/images',
+
+	// url to uploads
+	categoriesUploadUrl: '/images/categories',
+	productsUploadUrl: '/images/products',
+	filesUploadUrl: '',
+	themeAssetsUploadUrl: '/assets/images',
+
+	// store UI language
+	language: 'en',
+
+	// used by API
+	orderStartNumber: 1000,
+
+	developerMode: true
+};
