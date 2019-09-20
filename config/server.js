fdbad05eddb569d15f891a04b2446b3f54940dfd
jrsantur <jrsantur06@gmail.com>
2019-09-20 00:06:17 -0500

    dockefile upload

--- config/server.js
+++ config/server.js
@@ -12,10 +12,10 @@ const dbUrl =
 
 module.exports = {
 	// used by Store (server side)
-	apiBaseUrl: `http://localhost:3001/api/v1`,
+	apiBaseUrl: `/api/v1`,
 
 	// used by Store (server and client side)
-	ajaxBaseUrl: `http://localhost:3001/ajax`,
+	ajaxBaseUrl: `/ajax`,
 
 	// Access-Control-Allow-Origin
 	storeBaseUrl: '*',
@@ -23,8 +23,8 @@ module.exports = {
 	// used by API
 	adminLoginUrl: '/admin/login',
 
-	apiListenPort: 3001,
-	storeListenPort: 3000,
+	apiListenPort: 8081,
+	storeListenPort: 8080,
 
 	// used by API
 	mongodbServerUrl: dbUrl,
